a63d56f866b5d783d06d8844ecfba2c223e2e021
Razib Hasan <razibulhasan2003@gmail.com>
2025-12-27 04:36:53 +0200

    Run HTTP server

--- docker-homework/app.js
+++ docker-homework/app.js
@@ -1,1 +1,12 @@
-console.log("Hello from my Docker homework project!");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello from my Docker homework project!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
